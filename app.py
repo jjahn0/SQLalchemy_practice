dc193ff78b872e05fd0dfe4165cbe8c107737910
jjahn0 <35445039+jjahn0@users.noreply.github.com>
2018-09-18 16:41:49 -0700

    update app.py and template

--- app.py
+++ app.py
@@ -9,29 +9,34 @@ from flask import (
 
 from flask_sqlalchemy import SQLAlchemy
 
-from initdb import Rider, Station
+# from initdb import Rider, Station
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///GoBike.sqlite"
+# #################################################
+# # Database Setup
+# #################################################
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///GoBike.sqlite"
 
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# db = SQLAlchemy(app)
 
 # route for index.html - inital page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.rout("/api/gobike")
-def gobike():
-    results = db.session.query(Station.station_id, 
-                                Station.station_name, 
-                                Station.latitutde, 
-                                Station.longitude).all()
-    return 0
+# @app.route("/api/gobike")
+# def gobike():
+#     results = db.session.query(Station.station_id, 
+#                                 Station.station_name, 
+#                                 Station.latitutde, 
+#                                 Station.longitude).all()
+#     return 0
+
+if __name__ == "__main__":
+    app.run()
