6d54f2b2efd75fa9e4722af42a551c6d86748b7e
jjahn0 <35445039+jjahn0@users.noreply.github.com>
2018-09-20 15:47:40 -0700

    update to app.py - reconfiguation of Models

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ from flask import (
     request,
     redirect)
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
+
+import pymysql
+import pandas as pd
+pymysql.install_as_MySQLdb()
+
+Base = declarative_base()
+
 from flask_sqlalchemy import SQLAlchemy
 
 # from initdb import Rider, Station
@@ -16,27 +26,69 @@ from flask_sqlalchemy import SQLAlchemy
 #################################################
 app = Flask(__name__)
 
-# #################################################
-# # Database Setup
-# #################################################
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///GoBike.sqlite"
+#################################################
+# Database Setup
+#################################################
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///GoBike.sqlite"
 
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+class Rider(db.Model):
+    __tablename__ = 'rider'
+    id = Column(db.Integer, primary_key=True)
+    bike_id = Column(db.Integer, nullable=False)
+    start_time = Column(db.Integer, nullable=False)
+    start_station_id = Column(db.Integer)
+    end_time = Column(db.Integer, nullable=False)
+    end_station_id = Column(db.Integer)
+    duration = Column(db.Integer, nullable=False)
+    user_type = Column(db.Integer, nullable=True)
+    member_age = Column(db.Integer, nullable=True)
+    member_gender = Column(db.Integer, nullable=True)
+
+class Station(db.Model):
+    __tablename__ = 'station'
+    id = Column(db.Integer, primary_key=True)
+    station_id = Column(db.Integer, unique=True, nullable=False)
+    station_name = Column(db.String(255), unique=True, nullable=False)
+    latitude = Column(db.Float, nullable=False)
+    longitude = Column(db.Float, nullable=False)
+    neighborhood = Column(db.String(255))
+    zipcode = Column(db.Integer)
+
 
 # route for index.html - inital page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# @app.route("/api/gobike")
-# def gobike():
-#     results = db.session.query(Station.station_id, 
-#                                 Station.station_name, 
-#                                 Station.latitutde, 
-#                                 Station.longitude).all()
-#     return 0
+@app.route("/api/gobike")
+def gobike():
+    result = []
+    entry = {}
+    test = {
+        "id": 101,
+        "name": "Happy Gilmore",
+        "sport" : ["hockey","golf", "Fooseball"]
+    }
+    stationQuery = db.session.query(Station.station_id, 
+                                Station.station_name, 
+                                Station.latitude, 
+                                Station.longitude).all()
+    for _row in stationQuery:
+        entry = {
+            "station_id" : _row.station_id,
+            "station_name" : _row.station_name,
+            "latitude" : _row.latitude,
+            "longitude" : _row.longitude
+        }
+        result.append(entry)
+
+    # return jsonify(result)
+    return jsonify(test)
+        
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- initdb.py
+++ initdb.py
@@ -8,6 +8,13 @@ import pandas as pd
 pymysql.install_as_MySQLdb()
 
 Base = declarative_base()
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
 
 class Rider(Base):
     __tablename__ = 'rider'
